7db0bddef8402d8ad0af0c373fb57a64feee76a5
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-23 14:04:40 +0700

    mengisi array

--- Array.cpp
+++ Array.cpp
@@ -7,4 +7,10 @@ int main()
     int dataku [5] = {10, 20, 30, 40, 50};
     //deklarasi tanpa inisialisasi
     int datamu [5];
+
+    //cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku [1] << endl;
+    //mengisi array
+    dataku [1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku [1] << endl;
 }
